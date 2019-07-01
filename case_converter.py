ef2b9c93d879a68fc9822a4997d1aa80018ca7d8
ecly <ecly@mailbox.org>
2019-07-01 12:55:26 +0200

    add decoder implementation for leet, ultra leet and dank

--- case_converter.py
+++ case_converter.py
@@ -18,6 +18,7 @@ Example usage:
     ```
 """
 import re
+import random
 
 CAMEL_CASE = "CamelCase"
 PASCAL_CASE = CAMEL_CASE
@@ -46,6 +47,68 @@ _CASE_REGEX_MAP = {
 
 def _encode(text, case):
     return re.findall(case, text)
+
+
+def _dank_decode(encoded):
+    def dankify_word(w):
+        return "".join([c.upper() if random.random() > 0.5 else c.lower() for c in w])
+
+    return " ".join([dankify_word(w) for w in encoded])
+
+
+def _ultra_leet_decode(encoded):
+    # https://www.dcode.fr/leet-speak-1337
+    leet_dict = {
+        "A": ["4", "/\\", "@", "∂", "/-\\"],
+        "B": ["8", "13", "|3", "ß"],
+        "C": ["(", "¢", "<", "[", "©"],
+        "D": ["[)", "|>", "|)", "|]"],
+        "E": ["3", "€", "є", "[-"],
+        "F": ["|=", "ƒ", "/="],
+        "G": ["6", "(_+"],
+        "H": ["#", "/-/", "[-]", "]-[", ")-(", "(-)", ":-:", "|~|", "|-|", "]~[", "}{"],
+        "I": ["1", "!", "|", "][", "]", ":"],
+        "J": ["_|", "_/", "¿"],
+        "K": ["X", "|<", "|{", "ɮ"],
+        "L": ["£", "1_", "ℓ", "|_", "[_"],
+        "M": ["|V|", "|\\/|", "/\\/\\", "/V\\"],
+        "N": ["|V", "|\\|", "/\\/", "[\\]", "/V"],
+        "O": ["[]", "0", "()", "°"],
+        "P": ["|*", "|o", "|º", "|°", "/*"],
+        "Q": ["¶", "(_,)", "()_", "0_", "°|", "<|", "0."],
+        "R": ["2", "|?", "/2", "®", "Я", "|2"],
+        "S": ["5", "$", "§", "_/¯"],
+        "T": ["7", "†", "¯|¯"],
+        "U": ["(_)", "|_|", "L|", "µ"],
+        "V": ["\\/", "|/"],
+        "W": ["\\/\\/", "vv", "'//", "\\^/", "\\V/", "\\|/", "\\_|_/", "\\_:_/"],
+        "X": ["><", "}{", "×", ")("],
+        "Y": ["`/", "φ", "¥", "'/"],
+        "Z": ["≥", "7_", ">_"],
+    }
+    def leetify_word(w):
+        return "".join(random.choice(leet_dict.get(c.upper(), c)) for c in w)
+
+    return " ".join([leetify_word(w) for w in encoded])
+
+def _leet_decode(encoded):
+    leet_dict = {
+        "A": "4",
+        "B": "8",
+        "C": "(",
+        "E": "3",
+        "H": "#",
+        "I": "1",
+        "O": "0",
+        "R": "2",
+        "S": "5",
+        "T": "7",
+    }
+    def leetify_word(w):
+        return "".join(leet_dict.get(c.upper(), c) for c in w)
+
+    return " ".join([leetify_word(w) for w in encoded])
+
 
 
 def _decode(encoded, case):
@@ -79,9 +142,9 @@ def convert(text, case_from, case_to):
         Should be `str` representation of the case as found in `_CASE_REGEX_MAP`.
         Example: "CamelCase"
     """
-    assert (
-        case_from in _CASE_REGEX_MAP
-    ), "case_from should be one of: %s" % ", ".join(_CASE_REGEX_MAP.keys())
+    assert case_from in _CASE_REGEX_MAP, "case_from should be one of: %s" % ", ".join(
+        _CASE_REGEX_MAP.keys()
+    )
     assert case_to in _CASE_REGEX_MAP, "case_from should be one of: %s" % ", ".join(
         _CASE_REGEX_MAP.keys()
     )
