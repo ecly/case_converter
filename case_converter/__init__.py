3c4a06c93a13fbaf64a5f72aca21e6ba155a313a
Emil Lynegaard <eml@languagewire.com>
2022-09-12 21:11:04 +0200

    Refactor to package and classes

---
+++ case_converter/__init__.py
@@ -0,0 +1,43 @@
+"""
+Copyright (c) 2022 Emil Lynegaard
+Distributed under the MIT software license, see the
+accompanying LICENSE.md or https://opensource.org/licenses/MIT
+
+Convert between programming naming conventions.
+Supported cases are:
+    - camelCase (pascalCase)
+    - CamelCase (PascalCase)
+    - MACRO_CASE
+    - snake_case
+    - space case
+    - dank (target only)
+    - leet (target only)
+    - ultraleet (target only)
+
+The supported cases can also be listed with:
+    ```
+    # encoders
+    python -c "import case_converter; print(case_converter.ENCODERS.keys())"
+
+    # decoders
+    python -c "import case_converter; print(case_converter.DECODERS.keys())"
+
+
+Example usage:
+    ```
+    >>> from case_converter import convert
+    >>> convert("MyValue", "CamelCase", SNAKE_CASE")
+    'my_value'
+    >>> convert("fooBarBaz", "pascalCase", "MACRO_CASE")
+    'FOO_BAR_BAZ'
+    >>> convert("WHAT_IS_YOUR_NAME", "MACRO_CASE", "dank")
+    "WhAt iS yOUr NAMe"
+    >>> convert("I am very cool", "space case", "leet")
+    '1 4m v32y (00l'
+    ```
+"""
+import importlib
+import pkgutil
+
+from case_converter.case_converter import convert
+from case_converter.cases import DECODERS, ENCODERS, CaseEncoderDecoder
--- tests/test_case_converter.py
+++ tests/test_case_converter.py
@@ -5,15 +5,8 @@ accompanying LICENSE.md or https://opensource.org/licenses/MIT
 """
 
 import unittest
-from case_converter import (
-    convert,
-    CAMEL_CASE,
-    PASCAL_CASE,
-    LOWER_CAMEL_CASE,
-    LOWER_PASCAL_CASE,
-    MACRO_CASE,
-    SNAKE_CASE,
-)
+
+from case_converter import convert
 
 
 class TestCaseConverter(unittest.TestCase):
@@ -34,16 +27,11 @@ class TestCaseConverter(unittest.TestCase):
         self.assertEqual(convert("FOO_BAR", "MACRO_CASE", "CamelCase"), "FooBar")
         self.assertEqual(convert("FOO", "MACRO_CASE", "CamelCase"), "Foo")
 
-    def test_module_global_regex(self):
-        self.assertEqual(convert("FOO_BAR_BAZ", MACRO_CASE, PASCAL_CASE), "FooBarBaz")
-        self.assertEqual(convert("foo_bar", SNAKE_CASE, LOWER_CAMEL_CASE), "fooBar")
-        self.assertEqual(convert("Foo", CAMEL_CASE, LOWER_PASCAL_CASE), "foo")
-
     def test_bad_cases_raises_assertion_error(self):
-        with self.assertRaises(AssertionError):
+        with self.assertRaises(ValueError):
             convert("FooBar", "PASCAL_CASE", "unknown")
-            convert("FooBar", CAMEL_CASE, "unknown")
-            convert("FooBar", "unknown", SNAKE_CASE)
+            convert("FooBar", "CamelCase", "unknown")
+            convert("FooBar", "unknown", "snake_case")
 
 
 if __name__ == "__main__":
